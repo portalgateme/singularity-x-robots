0ba80675ab6ce217d959d869045fc0a59c0f3df8
nobody <nobody>
2024-07-10 06:31:45 +0000

    deal with error

--- src/xBot.py
+++ src/xBot.py
@@ -8,10 +8,17 @@ from dotenv import load_dotenv
 from database import create_pool, get_referral_code
 
 load_dotenv()
+log_filename = os.getenv('LOG_FILENAME', 'error.log')
 
 X_BEARER_TOKEN = os.getenv('X_BEARER_TOKEN')
 TARGET_X_ID = os.getenv('X_TWEET_ID')
 X_USER_ID = os.getenv('X_USER_ID')
+
+
+logging.basicConfig(level=logging.INFO, 
+                    format='%(asctime)s - %(levelname)s - %(message)s',
+                    filename=log_filename,
+                    filemode='a')
 
 auth = tweepy.OAuth2BearerHandler(X_BEARER_TOKEN)
 api = tweepy.API(auth)
@@ -40,28 +47,38 @@ class StreamListener(tweepy.StreamListener):
         self.pool = pool
 
     def on_status(self, status):
-        if hasattr(status, 'in_reply_to_status_id_str') and status.in_reply_to_status_id_str == TARGET_X_ID:
+        if hasattr(status, 'in_reply_to_status_id_str') and status. in_reply_to_status_id_str == TARGET_X_ID:
             asyncio.run(self.process_reply(status))
 
     async def process_reply(self, status):
         try:
             reply_message = await handle_reply(status, self.pool)
             if reply_message != "":
-                self.reply(status, reply_message)
                 api.update_status(status=reply_message, in_reply_to_status_id=status.id)
         except Exception as e:
-            print(f"Error on reply: {str(e)}")
+            raise e
 
     def on_error(self, status_code):
-        if status_code == 420:
-            return False
+        logging.error(f"Stream encountered error: {status_code}")
+        if status_code == 420 or status_code == 429:
+            self.retry_count += 1
+            wait_time = min(60 * 2 ** self.retry_count, 900)  # 最大 15 分钟
+            logging.info(f"Rate limit exceeded, retrying in {wait_time} seconds")
+            asyncio.sleep(wait_time)
+            return True  # 返回 True 以重新连接流
+        else:
+            return False  # 断开连接
 
-# 主函数
 async def main():
     pool = await create_pool()
-    myStreamListener = StreamListener(pool)
-    myStream = tweepy.Stream(auth=api.auth, listener=myStreamListener)
-    myStream.filter(follow=[X_USER_ID])
-
+    refLinkListner = StreamListener(pool)
+    stream = tweepy.Stream(auth=api.auth, listener=refLinkListner)
+    while True:
+        try:
+            stream.filter(follow=[X_USER_ID])
+        except Exception as e:
+            logging.error(f"Stream encountered error: {e}")
+            await asyncio.sleep(30)
+    
 if __name__ == "__main__":
     asyncio.run(main())
